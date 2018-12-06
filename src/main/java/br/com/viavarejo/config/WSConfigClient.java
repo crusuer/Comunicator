9c14fb98be4416a333c505b803585f2ec485f7b1
ROBSON PEREIRA DA SILVA <7700250520@viavarejo.com.br>
2018-12-06 14:37:45 -0200

    Criacao do Web Service de resposta assincrona

--- src/main/java/br/com/viavarejo/config/WSConfigClient.java
+++ src/main/java/br/com/viavarejo/config/WSConfigClient.java
@@ -1,12 +1,21 @@
 package br.com.viavarejo.config;
 
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 import org.springframework.ws.client.core.WebServiceTemplate;
+import org.springframework.ws.config.annotation.EnableWs;
+import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.transport.http.MessageDispatcherServlet;
+import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
+import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 
+@EnableWs
 @Configuration
-public class WSConfigClient {
+public class WSConfigClient extends WsConfigurerAdapter{
 
 	@Bean
 	public Jaxb2Marshaller marshaller() {
@@ -14,15 +23,30 @@ public class WSConfigClient {
 		marshaller.setContextPath("br.com.viavarejo.model");
 		return marshaller;
 	}
-	
+
 	@Bean
 	public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
 		WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
-		webServiceTemplate.setDefaultUri("https://cemissp.com.br/cemiseguranca/Integracao");//http://localhost:8113/ws/imeis.wsdl
+		webServiceTemplate.setDefaultUri("https://cemissp.com.br/cemiseguranca/Integracao");// http://localhost:8113/ws/imeis.wsdl
 		webServiceTemplate.setMarshaller(marshaller);
 		webServiceTemplate.setUnmarshaller(marshaller);
 		return webServiceTemplate;
-		
+
+	}
+
+	@Bean
+	public ServletRegistrationBean<?> messageDispatcherServlet(ApplicationContext applicationContext) {
+		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+		servlet.setApplicationContext(applicationContext);
+		servlet.setTransformWsdlLocations(true);
+		return new ServletRegistrationBean<>(servlet, "/ws/*");
+	}
+
+	@Bean(name = "orders")
+	public Wsdl11Definition defaultWsdl11Definition() {
+		SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+		wsdl11Definition.setWsdl(new ClassPathResource("/orders.wsdl"));
+		return wsdl11Definition;
 	}
 
 }
