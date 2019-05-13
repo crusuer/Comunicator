370b4a0515974abc8af7f81e07a4cc3b0a33f1e1
Robson <robson_mt@hotmail.com>
2019-05-12 23:07:52 -0300

    Atualizacao para swagger

--- src/main/java/br/com/viavarejo/config/WSConfigClient.java
+++ src/main/java/br/com/viavarejo/config/WSConfigClient.java
@@ -12,41 +12,57 @@ import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWs
 @Configuration
-public class WSConfigClient extends WsConfigurerAdapter{
+@EnableSwagger2
+public class WSConfigClient extends WsConfigurerAdapter {
 
-	@Bean
-	public Jaxb2Marshaller marshaller() {
-		Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
-		marshaller.setContextPath("br.com.viavarejo.model");
-		return marshaller;
-	}
+  @Bean
+  public Jaxb2Marshaller marshaller() {
+    Jaxb2Marshaller marshaller = new Jaxb2Marshaller();
+    marshaller.setContextPath("br.com.viavarejo.model");
+    return marshaller;
+  }
 
-	@Bean
-	public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
-		WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
-		webServiceTemplate.setDefaultUri("https://cemissp.com.br/cemiseguranca/Integracao");// http://localhost:8113/ws/imeis.wsdl
-		webServiceTemplate.setMarshaller(marshaller);
-		webServiceTemplate.setUnmarshaller(marshaller);
-		return webServiceTemplate;
+  @Bean
+  public WebServiceTemplate webServiceTemplate(Jaxb2Marshaller marshaller) {
+    WebServiceTemplate webServiceTemplate = new WebServiceTemplate();
+    webServiceTemplate.setDefaultUri(
+        "https://cemissp.com.br/cemiseguranca/Integracao");// http://localhost:8113/ws/imeis.wsdl
+    webServiceTemplate.setMarshaller(marshaller);
+    webServiceTemplate.setUnmarshaller(marshaller);
+    return webServiceTemplate;
 
-	}
+  }
 
-	@Bean
-	public ServletRegistrationBean<?> messageDispatcherServlet(ApplicationContext applicationContext) {
-		MessageDispatcherServlet servlet = new MessageDispatcherServlet();
-		servlet.setApplicationContext(applicationContext);
-		servlet.setTransformWsdlLocations(true);
-		return new ServletRegistrationBean<>(servlet, "/ws/*");
-	}
+  @Bean
+  public ServletRegistrationBean<?> messageDispatcherServlet(
+      ApplicationContext applicationContext) {
+    MessageDispatcherServlet servlet = new MessageDispatcherServlet();
+    servlet.setApplicationContext(applicationContext);
+    servlet.setTransformWsdlLocations(true);
+    return new ServletRegistrationBean<>(servlet, "/ws/*");
+  }
 
-	@Bean(name = "orders")
-	public Wsdl11Definition defaultWsdl11Definition() {
-		SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
-		wsdl11Definition.setWsdl(new ClassPathResource("/orders.wsdl"));
-		return wsdl11Definition;
-	}
+  /*@Bean(name = "orders")
+  public Wsdl11Definition defaultWsdl11Definition() {
+    SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
+    wsdl11Definition.setWsdl(new ClassPathResource("/orders.wsdl"));
+    return wsdl11Definition;
+  }*/
 
+  @Bean
+  public Docket api() {
+    return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.any())
+        .paths(PathSelectors.any())
+        .build();
+  }
 }
--- src/main/java/br/com/viavarejo/controller/ABRController.java
+++ src/main/java/br/com/viavarejo/controller/ABRController.java
@@ -1,5 +1,16 @@
 package br.com.viavarejo.controller;
 
+import br.com.viavarejo.model.BlockOrderUnboundWSInCompany;
+import br.com.viavarejo.model.BlockOrderWSOut;
+import br.com.viavarejo.model.CemisspSearchInCompany;
+import br.com.viavarejo.model.CemisspSearchOutCompany;
+import br.com.viavarejo.model.CreateBlockOrderUnboundServiceResponse;
+import br.com.viavarejo.model.GetStatusIMEIResponse;
+import br.com.viavarejo.model.GetStatusServiceResponse;
+import br.com.viavarejo.model.ImeiStatusOut;
+import br.com.viavarejo.model.UnlockBlockOrderServiceResponse;
+import br.com.viavarejo.model.UnlockWSInCompany;
+import br.com.viavarejo.service.ImeiClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,36 +18,33 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.viavarejo.ImeiClient;
-import br.com.viavarejo.model.BlockOrderUnboundWSInCompany;
-import br.com.viavarejo.model.BlockOrderWSOut;
-import br.com.viavarejo.model.CreateBlockOrderUnboundServiceResponse;
-import br.com.viavarejo.model.GetStatusIMEIResponse;
-import br.com.viavarejo.model.ImeiStatusOut;
-import br.com.viavarejo.model.UnlockBlockOrderServiceResponse;
-import br.com.viavarejo.model.UnlockWSInCompany;
-
 @RestController
 public class ABRController {
 
-	@Autowired
-	ImeiClient imeiClient;
+  @Autowired
+  ImeiClientService imeiClientService;
 
-	@RequestMapping(value = "status", method = RequestMethod.GET)
-	public ImeiStatusOut imeiStatus(@RequestParam(value = "imei") String imei) {
-		GetStatusIMEIResponse response = imeiClient.getImeiStatus(imei);
-		return response.getReturn();
-	}
+  @RequestMapping(value = "statusImei", method = RequestMethod.GET)
+  public ImeiStatusOut imeiStatus(@RequestParam(value = "imei") String imei) {
+    GetStatusIMEIResponse response = imeiClientService.getImeiStatus(imei);
+    return response.getReturn();
+  }
 
-	@RequestMapping(value = "block", method = RequestMethod.POST)
-	public BlockOrderWSOut imeiBlock(@RequestBody BlockOrderUnboundWSInCompany block) {
-		CreateBlockOrderUnboundServiceResponse response = imeiClient.lockBlockOrder(block);
-		return response.getReturn();
-	}
+  @RequestMapping(value = "block", method = RequestMethod.POST)
+  public BlockOrderWSOut imeiBlock(@RequestBody BlockOrderUnboundWSInCompany block) {
+    CreateBlockOrderUnboundServiceResponse response = imeiClientService.lockBlockOrder(block);
+    return response.getReturn();
+  }
 
-	@RequestMapping(value = "unblock", method = RequestMethod.POST)
-	public BlockOrderWSOut imeiUnblock(@RequestBody UnlockWSInCompany unblock) {
-		UnlockBlockOrderServiceResponse response = imeiClient.unlockBlockOrder(unblock);
-		return response.getReturn();
-	}
+  @RequestMapping(value = "unblock", method = RequestMethod.POST)
+  public BlockOrderWSOut imeiUnblock(@RequestBody UnlockWSInCompany unblock) {
+    UnlockBlockOrderServiceResponse response = imeiClientService.unlockBlockOrder(unblock);
+    return response.getReturn();
+  }
+
+  @RequestMapping(value = "statusService", method = RequestMethod.GET)
+  public CemisspSearchOutCompany serviceStatus(@RequestBody CemisspSearchInCompany service) {
+    GetStatusServiceResponse response = imeiClientService.getServiceStatus(service);
+    return response.getReturn();
+  }
 }
